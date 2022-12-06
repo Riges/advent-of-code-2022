d3f39c0bfea942c82a7b38b599aee99d595fc7ba
Riges <mail@riges.net>
2022-12-06 20:42:21 +0100

    day 4

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 #[allow(dead_code)]
 fn previous_days() {
     day01::day01().unwrap();
     day02::day02().unwrap();
+    day03::day03().unwrap();
 }
 
 fn main() {
-    day03::day03().unwrap();
+    day04::day04().unwrap();
 }
