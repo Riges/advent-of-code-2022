aad160c988fe9fef745e203990d8a42959462410
Riges <mail@riges.net>
2022-12-08 00:56:31 +0100

    Day 06 part 2

--- src/day06.rs
+++ src/day06.rs
@@ -48,9 +48,9 @@ fn subroutine(data_stream: &str, marker_size: usize) -> usize {
 
 pub fn day06() -> anyhow::Result<()>  {
   let data_stream = load_data_stream("data/day06.txt")?;
-  let result = subroutine(&data_stream, 4);
 
-  println!("Day 06: {}", result);
+  println!("Day 06 start-of-packet marker: {}", subroutine(&data_stream, 4));
+  println!("Day 06 messages: {}", subroutine(&data_stream, 14));
 
   Ok(())
 }
@@ -61,11 +61,20 @@ mod tests {
   use super::*;
 
   #[test]
-  fn should_return_subroutine() {
+  fn should_return_subroutine_of_marker() {
     assert_eq!(subroutine("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4), 7);
     assert_eq!(subroutine("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
     assert_eq!(subroutine("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
     assert_eq!(subroutine("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
     assert_eq!(subroutine("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",4), 11);
+  }
+
+  #[test]
+  fn should_return_subroutine_of_message() {
+    assert_eq!(subroutine("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), 19);
+    assert_eq!(subroutine("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
+    assert_eq!(subroutine("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
+    assert_eq!(subroutine("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
+    assert_eq!(subroutine("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
   }
 }
