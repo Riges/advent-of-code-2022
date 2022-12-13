be013a184a6c213856eff24938f832fddfebde7b
Riges <mail@riges.net>
2022-12-13 01:18:12 +0100

    Day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -3,7 +3,7 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-fn load_trees_v2(path: &str) -> anyhow::Result<Vec<Vec<i32>>> {
+fn load_trees(path: &str) -> anyhow::Result<Vec<Vec<i32>>> {
     let mut trees: Vec<Vec<i32>> = vec![];
     let file = File::open(path)?;
     let reader = BufReader::new(file);
@@ -20,8 +20,54 @@ fn load_trees_v2(path: &str) -> anyhow::Result<Vec<Vec<i32>>> {
     Ok(trees)
 }
 
+#[derive(Debug)]
+struct CrossnNeighbour {
+    up: Vec<i32>,
+    down: Vec<i32>,
+    left: Vec<i32>,
+    right: Vec<i32>,
+}
+
+fn get_cross_neighbour(trees: &[Vec<i32>], row: usize, col: usize) -> CrossnNeighbour {
+    CrossnNeighbour {
+        up: trees[0..row].iter().map(|r| r[col]).collect(),
+        down: trees[row + 1..].iter().map(|r| r[col]).collect(),
+        left: trees[row][0..col].to_vec(),
+        right: trees[row][col + 1..].to_vec(),
+    }
+}
+
+fn is_visible(tree: i32, other: i32) -> bool {
+    other >= tree
+}
+
 fn tree_is_visible(tree: i32, others: Vec<i32>) -> bool {
-    !others.iter().any(|t| *t >= tree)
+    !others.iter().any(|t| is_visible(tree, *t))
+}
+
+fn calculate_scenic_score(size: i32, others: Vec<i32>) -> i32 {
+    let mut cpt = 0;
+    for other in others.iter() {
+        cpt += 1;
+        if is_visible(size, *other) {
+            break;
+        }
+    }
+
+    cpt
+}
+
+fn calculate_tree_scenic_score(tree: i32, cross_neighbour: CrossnNeighbour) -> i32 {
+    let mut cross_neighbour = cross_neighbour;
+    cross_neighbour.up.reverse();
+    cross_neighbour.left.reverse();
+    let mut cpt = 0;
+    cpt += calculate_scenic_score(tree, cross_neighbour.up);
+    cpt *= calculate_scenic_score(tree, cross_neighbour.left);
+    cpt *= calculate_scenic_score(tree, cross_neighbour.down);
+    cpt *= calculate_scenic_score(tree, cross_neighbour.right);
+
+    cpt
 }
 
 fn count_visible_trees(trees: &Vec<Vec<i32>>) -> i32 {
@@ -37,27 +83,13 @@ fn count_visible_trees(trees: &Vec<Vec<i32>>) -> i32 {
                 continue;
             }
 
-            let previous_col: Vec<i32> = trees[row][0..col].to_vec();
-            if tree_is_visible(*tree, previous_col) {
-                count += 1;
-                continue;
-            }
+            let cross_neighbour = get_cross_neighbour(trees, row, col);
 
-            let next_col: Vec<i32> = trees[row][col + 1..].to_vec();
-            if tree_is_visible(*tree, next_col) {
-                count += 1;
-                continue;
-            }
-
-            let previous_rows_in_same_col: Vec<i32> =
-                trees[0..row].iter().map(|r| r[col]).collect();
-            if tree_is_visible(*tree, previous_rows_in_same_col) {
-                count += 1;
-                continue;
-            }
-
-            let next_rows_in_same_col: Vec<i32> = trees[row + 1..].iter().map(|r| r[col]).collect();
-            if tree_is_visible(*tree, next_rows_in_same_col) {
+            if tree_is_visible(*tree, cross_neighbour.up)
+                || tree_is_visible(*tree, cross_neighbour.down)
+                || tree_is_visible(*tree, cross_neighbour.left)
+                || tree_is_visible(*tree, cross_neighbour.right)
+            {
                 count += 1;
                 continue;
             }
@@ -67,9 +99,58 @@ fn count_visible_trees(trees: &Vec<Vec<i32>>) -> i32 {
     count
 }
 
+fn highest_scenic_score(trees: &[Vec<i32>]) -> i32 {
+    let mut highest = 0;
+    for (row, line) in trees.iter().enumerate() {
+        for (col, tree) in line.iter().enumerate() {
+            let cross_neighbour = get_cross_neighbour(trees, row, col);
+            let score = calculate_tree_scenic_score(*tree, cross_neighbour);
+            if score > highest {
+                highest = score;
+            }
+        }
+    }
+
+    highest
+}
+
 pub fn day08() -> anyhow::Result<()> {
-    let trees_v2 = load_trees_v2("data/day08.txt")?;
-    println!("Day 08 part 2 - {}", count_visible_trees(&trees_v2));
+    let trees_v2 = load_trees("data/day08.txt")?;
+    println!("Day 08 part 1 - {}", count_visible_trees(&trees_v2));
+    println!("Day 08 part 2 - {}", highest_scenic_score(&trees_v2));
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn calculate_tree_scenic_score_with_sample() {
+        let score = calculate_tree_scenic_score(
+            5,
+            CrossnNeighbour {
+                up: vec![3],
+                down: vec![4, 5, 3],
+                left: vec![2, 5],
+                right: vec![1, 2],
+            },
+        );
+
+        assert_eq!(score, 4);
+
+        let score = calculate_tree_scenic_score(
+            5,
+            CrossnNeighbour {
+                up: vec![3, 5, 3],
+                down: vec![3],
+                left: vec![3, 3],
+                right: vec![4, 9],
+            },
+        );
+
+        assert_eq!(score, 8);
+    }
 }
